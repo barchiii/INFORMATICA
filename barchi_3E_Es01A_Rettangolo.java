f72d9cdcfcd7a4f502863eeca36da84b3ebef67c
barchiii <145505905+barchiii@users.noreply.github.com>
2024-01-09 11:52:11 +0100

    Exercise 1 commented

--- barchi_3E_Es01A_Rettangolo.java
+++ barchi_3E_Es01A_Rettangolo.java
@@ -7,21 +7,27 @@ public class barchi_3E_Es01A_Rettangolo {
         double base;
         double area;
         double perimetro;
+        
         System.out.println("Inserisci il valore dell'altezza");
         altezza=keyboard.nextDouble();
+
+        // check
         if(altezza>0)
         {
             System.out.println("Inserisci il valore della base");
             base=keyboard.nextDouble();
 
+            // calculations
             area=base*altezza;
             perimetro=(base+altezza)*2;
 
+            // output 1
             System.out.println("L'area del rettangolo è: "+area);
             System.out.println("Il perimetro del rettangolo è: "+perimetro);
         }
         else
         {
+            // output2
             System.out.println("Hai inserito un valore dell'altezza errato");
         }
     }
